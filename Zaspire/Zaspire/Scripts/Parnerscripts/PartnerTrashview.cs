85823a8f19a949081e3ecb06fafc43f9149bc832
kedikakavitha <kedikakavitha11@gmail.com>
2016-01-21 12:46:32 +0530

    Scripts and helpers locators

---
+++ Zaspire/Zaspire/Scripts/Parnerscripts/PartnerTrashview.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zaspire.PageHelpers;
+using Zaspire.PageHelpers.Com;
+using OpenQA.Selenium;
+
+namespace Zaspire.Scripts
+{
+    [TestClass]
+    public class ParntnersTrashView : DriverTestCase
+    {
+        [TestMethod]
+        public void TrashPartnerview()
+        {
+            string[] username = null;
+            string[] password = null;
+
+            var oXMLData = new XMLParse();
+            oXMLData.LoadXML("../../Config/OrganizationSetting.xml");
+
+            username = oXMLData.getData("settings/Credentials", "username");
+            password = oXMLData.getData("settings/Credentials", "password");
+
+            //Initializing the objects
+            var loginHelper = new LoginHelper(GetWebDriver());
+            var addpartnerHelper = new PartnerHelper(GetWebDriver());
+
+
+            //Login with valid username and password
+            Login(username[0], password[0]);
+            Console.WriteLine("Logged in as: " + username[0] + " / " + password[0]);
+
+            //Verify Page title
+            VerifyTitle("Dashboard");
+            Console.WriteLine("Redirected at Dashboard screen.");
+
+            //Click on Dashboard in menu
+            addpartnerHelper.ClickElement("partners");
+
+            //#############trash index###############
+
+            //Click on Trash
+            addpartnerHelper.ClickElement("Trash");
+
+            //Redirect to logout
+            GetWebDriver().Navigate().GoToUrl("http://www.zaspire.com/infoaspire/partners/trash");
+
+            //Click on view record
+            addpartnerHelper.ClickElement("TrashPartnerview");
+
+            //Clcik on activity tab
+            addpartnerHelper.ClickElement("Activitiestab");
+
+            //Click on Timline
+            //addpartnerHelper.ClickElement("Timelinetab");
+
+
+            //Click on contact
+            addpartnerHelper.ClickElement("Contacttab");
+
+            //Click on leads
+            addpartnerHelper.ClickElement("Relatedleadstab");
+
+            // Click on accounts
+            addpartnerHelper.ClickElement("Relatedaccounts");
+        }
+    }
+}
---
+++ Zaspire/Zaspire/Scripts/Vendorscripts/Addtasktovendor.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zaspire.PageHelpers;
+using Zaspire.PageHelpers.Com;
+using OpenQA.Selenium;
+
+namespace Zaspire.Scripts
+{
+    [TestClass]
+    public class AddtasktoVendor : DriverTestCase
+    {
+        [TestMethod]
+        public void Vendortask()
+        {
+            string[] username = null;
+            string[] password = null;
+
+            var oXMLData = new XMLParse();
+            oXMLData.LoadXML("../../Config/OrganizationSetting.xml");
+
+            username = oXMLData.getData("settings/Credentials", "username");
+            password = oXMLData.getData("settings/Credentials", "password");
+
+            //Initializing the objects
+            var loginHelper = new LoginHelper(GetWebDriver());
+            var VendorHelper = new VendorHelper(GetWebDriver());
+
+            //Variable
+
+            var FirstName = "mark" + RandomNumber(1, 99);
+            var LastName = "master" + RandomNumber(1, 99);
+            var Number = "12345678" + RandomNumber(10, 99);
+            var Partnername = "Partner" + RandomNumber(1, 20);
+            var Companyname = "Company" + RandomNumber(1, 10);
+            var Startdate = "2015-12-" + RandomNumber(1, 30);
+            var Enddate = "2015-12-" + RandomNumber(1, 30);
+
+
+            //Login with valid username and password
+            Login(username[0], password[0]);
+            Console.WriteLine("Logged in as: " + username[0] + " / " + password[0]);
+
+            //Verify Page title
+            VerifyTitle("Dashboard");
+            Console.WriteLine("Redirected at Dashboard screen.");
+
+            //Click on Dashboard in menu
+            VendorHelper.ClickElement("Vendor");
+
+            //############### Add Appmentoint ################################################
+
+            //Click On record
+            VendorHelper.ClickElement("VendorView");
+
+            //Click on more action
+            VendorHelper.MouseOver("VedorMoreactions");
+
+            //Select Add task
+            VendorHelper.ClickElement("Add task");
+
+            //Select type
+            VendorHelper.Select("EventType", "Task");
+
+            //Enter Location
+            VendorHelper.TypeText("Location", "Secunderabad");
+
+            //Enter  Subject
+            VendorHelper.TypeText("Subject", "Add appointment");
+
+            //Select start date
+            VendorHelper.Select("StartDate", Startdate);
+
+            //Click On record
+            VendorHelper.ClickElement("starttime");
+
+
+            //Select Moreaction
+            VendorHelper.Select("EndDate", Enddate);
+
+            //Select Add task
+            VendorHelper.ClickElement("Endtime");
+
+            //Enter Status
+            VendorHelper.ClickElement("Reminders");
+            
+            //Select Category
+            VendorHelper.Select("Popup", "1800");
+
+            //Select a file
+            VendorHelper.ClickElement("Email");
+
+            //Select a file
+            VendorHelper.Select("ReminderTime", "1800");
+
+            //Click on Recurring  button
+            VendorHelper.ClickElement("Recurring");
+
+            // Select a file
+            VendorHelper.Select("RepeteonceinEvery", "1");
+
+            // Select Type
+            VendorHelper.Select("RepeatType", "Monthly");
+
+            //Click on Check box sunday
+            //addpartnerHelper.ClickElement("WeekUI");
+
+            //Select a file
+            VendorHelper.ClickElement("OnRadiobutton");
+
+            //Click on Recurring  button
+            VendorHelper.ClickElement("Reportsofmonth");
+
+            // Select a file
+            VendorHelper.TypeText("Dateofthemonth", "2");
+
+            //Enter description
+            VendorHelper.ClickElement("OnRadiobutton");
+
+
+            // Select a file
+            VendorHelper.Select("Daytype", "First");
+
+            //Enter description
+            VendorHelper.Select("MonthDay", "2");
+
+
+            //Select on Save button
+            VendorHelper.Select("Until", "30/10/2015");
+
+            //Select Moreaction
+            VendorHelper.ClickElement("Save");
+
+
+        }
+    }
+}
